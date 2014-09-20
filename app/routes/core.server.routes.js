44d739c9ebc21a9ca9a225e39a418af5252dfa39
Pritesh Gupta <connect@priteshgupta.com>
2014-09-20 04:53:38 -0400

    Another commit! yay!!

--- app/routes/core.server.routes.js
+++ app/routes/core.server.routes.js
@@ -4,4 +4,10 @@ module.exports = function(app) {
 	// Root routing
 	var core = require('../../app/controllers/core');
 	app.route('/').get(core.index);
+	app.post('/test', function (req, res, next) {
+		console.log(req.body);
+		console.log(req.params);
+		console.log(req.query);
+		res.send('Hello World');
+	});
 };
